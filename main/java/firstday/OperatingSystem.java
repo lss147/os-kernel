28957b81b8c70317294b4bdf79020c80ca9b6a8a
lianshaoshan <1476469443@qq.com>
2020-09-09 20:08:02 +0800

    第三次提交

--- main/java/firstday/OperatingSystem.java
+++ main/java/firstday/OperatingSystem.java
@@ -13,8 +13,35 @@ import java.util.ArrayList;
 public class OperatingSystem {
 
     private Floppy floppyDisk = new Floppy();
+    private int  MAX_SECTOR_NUM = 18;
 
+    private void writeFileToFloppy(String fileName, boolean bootable, int cylinder,int beginSec) {
+        File file = new File(fileName);
+        InputStream in = null;
 
+        try {
+            in = new FileInputStream(file);
+            byte[] buf = new byte[512];
+            if (bootable) {
+                buf[510] = 0x55;
+                buf[511] = (byte) 0xaa;
+            }
+
+            while (in.read(buf) > 0) {
+                //将内核读入到磁盘第0面，第0柱面，第1个扇区
+                floppyDisk.writeFloppy(Floppy.MAGNETIC_HEAD.MAGNETIC_HEAD_0, cylinder, beginSec, buf);
+                beginSec++;
+
+                if (beginSec > MAX_SECTOR_NUM) {
+                    beginSec = 1;
+                    cylinder++;
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
 
     private void writeFileToFloppy(String fileName) {
         File file = new File(fileName);
@@ -40,6 +67,8 @@ public class OperatingSystem {
     }
 
     public void makeFllopy()   {
+        writeFileToFloppy("kernel.bat", false, 1, 2);
+
         floppyDisk.makeFloppy("system.img");
     }
 
